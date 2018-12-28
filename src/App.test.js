b18ca033f8ce7b5657f413cb8c5a10ea67599ad5
Tsaqif Setyo <tsaqiftrooper@gmail.com>
2018-12-28 15:56:08 +0700

    .

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Menu from './Menu';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.render(<Menu />, div);
 });
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Menu.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Hom">
+        <footer className="Hom-footer">
+          <div> ini footer</div>
+          <div> COPYRIGHT 2018 </div>
+        </footer>
+      </div>
+    );
+  }
+}
+export default Footer;
